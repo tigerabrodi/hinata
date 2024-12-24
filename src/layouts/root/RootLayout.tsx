ed32b9b76a85c5e302432f1bd42cba93cf7693e8
Tiger Abrodi <tigerabrodi@gmail.com>
2024-12-24 16:38:53 +0100

    feat: implement link logo

--- src/layouts/root/RootLayout.tsx
+++ src/layouts/root/RootLayout.tsx
@@ -1,9 +1,17 @@
-import { Outlet } from 'react-router'
+import { Link, Outlet } from 'react-router'
 import { SearchForm } from './SearchForm'
+import { ROUTES } from '@/lib/constants'
 
 export function RootLayout() {
   return (
     <div className="relative flex min-h-full w-full flex-col">
+      <Link
+        to={ROUTES.home}
+        aria-label="Hinata"
+        className="absolute left-6 top-6 hidden text-2xl font-bold italic md:inline"
+      >
+        H
+      </Link>
       <SearchForm />
       <Outlet />
     </div>
