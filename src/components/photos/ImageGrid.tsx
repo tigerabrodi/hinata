dcf1ff3eb4390814fa19bf1aaf0eb6df814b3b38
Tiger Abrodi <tigerabrodi@gmail.com>
2024-12-25 17:30:58 +0100

    refactor: cleanup image grid

--- src/components/photos/ImageGrid.tsx
+++ src/components/photos/ImageGrid.tsx
@@ -16,7 +16,7 @@ export function ImageGrid({
   const isDesktop = useMediaQuery(breakpoints.md)
 
   return (
-    <div className="grid grid-cols-1 grid-rows-[0px] gap-[18px] md:grid-cols-2 md:gap-4 lg:grid-cols-3">
+    <div className="grid grid-cols-1 grid-rows-[0px] gap-4 md:grid-cols-2 lg:grid-cols-3">
       {images.map((data, index) => {
         // On mobile we show a single column layout
         const isImageAmongFirstResults = index < 3
